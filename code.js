6088d4575c8978f9605517c8c6f966c920a6119b
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-04-19 18:40:44 -0400

    Create a random number function to generate rock, paper, or scissors

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+function computerPlay () {
+    let randoNum = Math.floor(Math.random() * 3) + 1;
+    if(randoNum === 1) {
+        return 'rock';;
+    } else if (randoNum === 2) {
+        return 'paper';
+    } else if (randoNum === 3) {
+        return 'scissors';
+    }
+}
