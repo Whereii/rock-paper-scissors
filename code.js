442fd28dac8623be28ac71283c95ae899085cbf3
JoshuaMcCausey <joshuamccausey@gmail.com>
2022-04-19 20:05:12 -0400

    Create a game function that playes 5 rounds of player vs computer and final score

--- code.js
+++ code.js
@@ -8,3 +8,54 @@ function computerPlay () {
         return 'scissors';
     }
 }
+
+//Mathes the player input to the random answer from the robot. Took the long way to solve this little problem. Im sure there are much quicker ways to create the solution to this one.
+function faceOff (player, robot) {
+    if (player === 'rock' && robot === 'scissors') {
+        return 'You win! Rock beats scissors';
+    } else if (player === 'rock' && robot === 'rock') {
+        return 'You tie! You both guessed rock';
+    } else if (player === 'rock' && robot === 'paper') {
+        return 'You lose. You guessed rock and the robot guessed paper';
+    } else if (player === 'paper' && robot === 'paper') {
+        return 'You tie. Looks like you both guessed paper. Its a draw';
+    } else if (player === 'paper' && robot === 'rock') {
+        return 'You win! paper beats rock';
+    } else if (player === 'paper' && robot === 'scissors') {
+        return 'You lose. Yikes. Better luck next time. The robot guessed scissors';
+    } else if (player === 'scissors' && robot === 'paper') {
+        return 'You Win! Looks like you dominated. You guessed scissors which beats the robots thin paper';
+    } else if (player === 'scissors' && robot === 'scissors') {
+        return 'You tie. You both guessed scissors';
+    } else if (player === 'scissors' && robot === 'rock') {
+        return 'You lose. What a beatdown! The robot guessed rock which beat your scissors';
+    }
+}
+
+//This little piece of code is very confusing and I should have broken it down into at least another "score" function. Found myself a little too deep and ended up just putting it all together
+function game () {
+    let computerScore = 0;
+    let playerScore = 0;
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Pick rock, paper, or scissors').toLowerCase();
+        //Decided to turn the faceOff function into a variable so it is a little easier for me to wrap my head around and to use .includes a little easier.
+        let round = faceOff(playerSelection, computerPlay());
+        console.log(round);
+        if(round.includes('win') === true) {
+            playerScore ++
+        } else if (round.includes('lose') === true) {
+            computerScore++
+        }
+        console.log('The computer score is ' + computerScore + '. Your score is ' + playerScore);
+    };
+    if (computerScore > playerScore) {
+        return 'The computer is the champion. The final score is Computer: ' + computerScore + ' Player: ' + playerScore;
+    } else if (computerScore === playerScore) {
+        return 'BOOOO the battle ended in a tie. How boring. The final score is Computer: ' + computerScore + ' Player: ' + playerScore;
+    } else {
+        return 'You are the victor of this battle. The final score is Computer: ' + computerScore + ' Player: ' + playerScore;
+    }
+}
+
+
+console.log(game());
